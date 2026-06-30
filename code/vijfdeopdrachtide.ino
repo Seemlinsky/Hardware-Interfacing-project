2e29db575dc31e07cdc48c579eb5855b6dee3e27
Seemlinsky <55606418+Seemlinsky@users.noreply.github.com>
2026-06-30 16:03:18 +0200

    documentatie en opdracht 3, 4, 5 & 6 verbeterd
    als volgt gedaan:
    - Ω-waardes toegevoegd in de documentatie.
    - Tinkercad-afbeeldingen van opdracht 3 en 4 verbeterd.
    - Opdracht 5 en 6 fysiek en in code verbeterd.
    - Code van opdracht 5 en 6 aangepast op functionaliteit en timing.

--- code/vijfdeopdrachtide.ino
+++ code/vijfdeopdrachtide.ino
@@ -1,67 +1,92 @@
 #include <Servo.h>
 
+// Hier maak ik de servo aan
 Servo mijnServo;
 
+// Hier zet ik welke pinnen ik gebruik
 const int servoPin = 9;
 const int knop1Pin = 2;
 const int knop2Pin = 3;
 
 void setup() {
-  // Servo koppelen aan pin 9
+  // Servo zit op pin 9
   mijnServo.attach(servoPin);
 
-  // Knoppen instellen als input
+  // Ik gebruik normale INPUT omdat ik zelf 10k weerstanden gebruik
   pinMode(knop1Pin, INPUT);
   pinMode(knop2Pin, INPUT);
 
   // Servo begint op 0 graden
   mijnServo.write(0);
+  delay(500);
 }
 
 void loop() {
-  // Knoppen uitlezen
-  int knop1Status = digitalRead(knop1Pin);
-  int knop2Status = digitalRead(knop2Pin);
+  // Bij pull-down weerstand is HIGH = knop ingedrukt
+  bool knop1 = digitalRead(knop1Pin) == HIGH;
+  bool knop2 = digitalRead(knop2Pin) == HIGH;
 
-  // Beide knoppen tegelijk ingedrukt
-  if (knop1Status == HIGH && knop2Status == HIGH) {
-    beweegServo(0, 120, 1000);
-    delay(2000); // even blijven staan
-    beweegServo(120, 0, 1000);
-  }
+  // Als ik een knop indruk, wacht Arduino even
+  // Dan kan hij beter zien of ik 1 knop of 2 knoppen indruk
+  if (knop1 || knop2) {
+    delay(300);
 
-  // Alleen knop 1 ingedrukt: langzamer bewegen
-  else if (knop1Status == HIGH) {
-    beweegServo(0, 120, 1000);
-    beweegServo(120, 0, 1000);
-  }
+    knop1 = digitalRead(knop1Pin) == HIGH;
+    knop2 = digitalRead(knop2Pin) == HIGH;
 
-  // Alleen knop 2 ingedrukt: sneller bewegen
-  else if (knop2Status == HIGH) {
-    beweegServo(0, 120, 500);
-    beweegServo(120, 0, 500);
+    // Beide knoppen tegelijk
+    if (knop1 && knop2) {
+      beweegServo(0, 120, 1000);
+      delay(2000);
+      beweegServo(120, 0, 1000);
+    }
+
+    // Alleen knop 1
+    else if (knop1) {
+      beweegServo(0, 120, 1000);
+      beweegServo(120, 0, 1000);
+    }
+
+    // Alleen knop 2
+    else if (knop2) {
+      beweegServo(0, 120, 500);
+      beweegServo(120, 0, 500);
+    }
+
+    // Wachten tot ik alle knoppen loslaat
+    wachtTotKnoppenLos();
+
+    // Servo blijft op 0 graden
+    mijnServo.write(0);
   }
 }
 
+// Deze functie laat de servo bewegen
 void beweegServo(int startHoek, int eindHoek, int totaleTijd) {
   int stappen = abs(eindHoek - startHoek);
 
-  // Tijd per stap berekenen, zodat de beweging ongeveer klopt
+  if (stappen == 0) {
+    return;
+  }
+
   int wachttijd = totaleTijd / stappen;
 
-  // Servo beweegt omhoog
   if (startHoek < eindHoek) {
     for (int hoek = startHoek; hoek <= eindHoek; hoek++) {
       mijnServo.write(hoek);
       delay(wachttijd);
     }
-  }
-
-  // Servo beweegt terug omlaag
-  else {
+  } else {
     for (int hoek = startHoek; hoek >= eindHoek; hoek--) {
       mijnServo.write(hoek);
       delay(wachttijd);
     }
+  }
+}
+
+// Wachten tot beide knoppen los zijn
+void wachtTotKnoppenLos() {
+  while (digitalRead(knop1Pin) == HIGH || digitalRead(knop2Pin) == HIGH) {
+    delay(10);
   }
 }
--- code/zesdeopdrachtide.ino
+++ code/zesdeopdrachtide.ino
@@ -6,60 +6,74 @@ const int servoPin = 9;
 const int xPin = A0;      // VRx van joystick
 const int knopPin = 2;    // SW van joystick
 
-int joystickWaarde = 0;
-int servoHoek = 0;
-int knopStatus = HIGH;
+int servoHoek = 90;
+bool knopAlGedaan = false;
 
 void setup() {
-  // Servo koppelen aan pin 9
   mijnServo.attach(servoPin);
 
-  // Ik gebruik de interne pull-up voor de joystickknop
+  // Joystickknop met interne pull-up weerstand
   pinMode(knopPin, INPUT_PULLUP);
 
-  // Servo begint op 0 graden
-  mijnServo.write(0);
+  // Servo begint in het midden
+  mijnServo.write(servoHoek);
+  delay(500);
 }
 
 void loop() {
-  // Joystickknop uitlezen
-  knopStatus = digitalRead(knopPin);
+  int knopStatus = digitalRead(knopPin);
 
-  // Als ik de joystick indruk, doet de servo een vaste beweging
-  if (knopStatus == LOW) {
+  // Als joystick wordt ingedrukt: automatische beweging 1 keer uitvoeren
+  if (knopStatus == LOW && knopAlGedaan == false) {
+    knopAlGedaan = true;
+
+    // Eerst naar 0 graden
+    beweegServo(servoHoek, 0, 500);
+
+    // Daarna opdrachtbeweging: 0 naar 120 en terug naar 0
     beweegServo(0, 120, 1000);
     beweegServo(120, 0, 1000);
+
+    servoHoek = 0;
   }
 
-  else {
-    // Links/rechts beweging van de joystick uitlezen
-    joystickWaarde = analogRead(xPin);
+  // Als knop los is, mag de joystick weer normaal sturen
+  if (knopStatus == HIGH) {
+    knopAlGedaan = false;
 
-    // Joystickwaarde omzetten naar een servohoek
-    servoHoek = map(joystickWaarde, 0, 1023, 0, 180);
+    int joystickWaarde = analogRead(xPin);
 
-    // Servo naar de berekende hoek sturen
+    // Dode zone zodat de servo niet raar trilt rond het midden
+    if (joystickWaarde < 450) {
+      servoHoek = 0;
+    } 
+    else if (joystickWaarde > 570) {
+      servoHoek = 180;
+    } 
+    else {
+      servoHoek = 90;
+    }
+
     mijnServo.write(servoHoek);
-
-    delay(15);
+    delay(30);
   }
 }
 
 void beweegServo(int startHoek, int eindHoek, int totaleTijd) {
   int stappen = abs(eindHoek - startHoek);
 
-  // Tijd per stap berekenen, zodat de beweging ongeveer klopt
+  if (stappen == 0) {
+    return;
+  }
+
   int wachttijd = totaleTijd / stappen;
 
-  // Servo beweegt omhoog
   if (startHoek < eindHoek) {
     for (int hoek = startHoek; hoek <= eindHoek; hoek++) {
       mijnServo.write(hoek);
       delay(wachttijd);
     }
-  }
-
-  // Servo beweegt terug omlaag
+  } 
   else {
     for (int hoek = startHoek; hoek >= eindHoek; hoek--) {
       mijnServo.write(hoek);
